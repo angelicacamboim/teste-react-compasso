dfdd60dd02cbd934a1ba167037f7fe2e537dfaef
Angelica Bayer <angelicacamboim@gmail.com>
2021-03-08 18:48:55 -0300

    ui material added

---
+++ nytimes/src/components/Blog.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Header from './Header';
+import FeaturedPost from './FeaturedPost';
+// import Footer from './Footer';
+
+
+const sections = [
+  { title: 'Technology', url: '#' },
+  { title: 'Science', url: '#' },
+];
+
+
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+];
+
+
+ 
+export default function Blog() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header sections={sections} />
+        <h1>Technology</h1>
+        <main>
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+         
+        </main>
+      </Container>
+    </React.Fragment>
+  );
+}
